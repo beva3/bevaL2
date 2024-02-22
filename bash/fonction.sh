c8a2da932867f5cf04e49fe5f3d09b8f5e86ed5a
beva3 <mizabienvenu@gmail.com>
2024-02-22 09:29:01 +0300

    repoL2

---
+++ bash/fonction.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+function bonjour {
+    echo "hello World"
+}
+
+bonjour
+bonjour
---
+++ classePersonne.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+class Personne{
+public:
+    Personne(){}
+    ~Personne(){}
+}
+
+int main()
+{
+
+    return 0;
+}
