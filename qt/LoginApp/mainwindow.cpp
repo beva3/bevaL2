f5ac93f9467ea57c9290f7a1264121a9f2595ca8
beva3 <mizabienvenu@gmail.com>
2024-02-24 04:48:25 +0300

    verify the user singin

--- qt/LoginApp/mainwindow.cpp
+++ qt/LoginApp/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QString>
+#include <QMessageBox>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +15,16 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_login_clicked()
+{
+    QString username = ui->lineEdit_setUsername->text();
+    QString passwd = ui->lineEdit_setPasswd->text();
+
+    if(username == "test" && passwd == "test"){
+        QMessageBox::information(this,"information","successful");
+    }else{
+        QMessageBox::warning(this,"warning","again");
+    }
+}
+
