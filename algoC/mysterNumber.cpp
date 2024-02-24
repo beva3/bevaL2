ad8ca600468a591c7204723de8ce002f91a522ed
beva3 <mizabienvenu@gmail.com>
2024-02-24 19:39:45 +0300

    prototype if the game

---
+++ algoC/mysterNumber.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iterator>
+#include <ostream>
+
+
+int main(){
+   int mysteryNumber = 75;
+   int maxTry = 6;
+   int number = 0;
+
+   for(int i = 0 ; i < maxTry ; i++){
+        std::cout << "Entrez le nombre entre 0 et 100 " ;
+        std::cin >> number ;
+        std::cin.ignore();
+        
+        if(mysteryNumber == number ){
+            std::cout << "Bravo" << std::endl;
+            break;
+        }
+        else if (number > mysteryNumber) {
+            std::cout << "trop grand"<<std::endl;
+        }else {
+            std::cout << "Trop Petit" << std::endl;
+        }
+
+        if(i == maxTry - 1){
+            std::cout << "Echouer !!" << std::endl;
+        }
+   }
+
+    return 0;
+}
