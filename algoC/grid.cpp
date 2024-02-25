89ad34f47ddf9ea742c03f0c359b09991de9d57e
beva3 <mizabienvenu@gmail.com>
2024-02-25 20:40:14 +0300

    set defaulte value

--- algoC/grid.cpp
+++ algoC/grid.cpp
@@ -7,6 +7,7 @@ typedef struct Size{
 } Size;
 
 void dislayGrid(int** grid, Size size);
+void defaulteValue(int** grid, Size size , int value);
 
 int main(){
     int x = 5, y = 5;
@@ -20,6 +21,11 @@ int main(){
     */
     dislayGrid(grid, {5,5});
 
+    cout << "defaulte value with 3 ...." << endl ;
+
+    defaulteValue(grid, {5,5}, 3);
+    dislayGrid(grid, {5,5});
+
     return 0;
 }
 
@@ -29,5 +35,12 @@ void dislayGrid(int** grid, Size size){
             cout << grid[i][j] << " " ;
         }
         cout << endl;
+    }
+}
+void defaulteValue(int** grid, Size size , int value){    
+    for (int i = 0 ; i < size.y; i++) {
+        for (int j = 0 ; j < size.x; j++) {
+            grid[i][j]  = value ;
+        }
     }
 }
